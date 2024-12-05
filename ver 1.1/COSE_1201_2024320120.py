6fd8cfefe1843e57a12ed99b0d7fb639341cbf95
Hong Donghwan <colin12131@gmail.com>
2024-12-05 17:51:04 +0900

    np배열로 전달3트

--- ver 1.1/COSE_1201_2024320120.py
+++ ver 1.1/COSE_1201_2024320120.py
@@ -36,16 +36,3 @@ def create_array(Data,subject,key):
             result.append(type[idx])
 
     return np.array(result)
-
-def read_file(name) :
-    f = open(name, "r", encoding = 'cp949')
-    read = csv.reader(f)
-    read = list(read)
-    return read
-
-def select_year():
-    num = int(input('1. 2020 / 2. 2021 / 3. 2022 / 4. 2023 중 그래프로 표현할 연도의 번호를 고르세요: '))
-    while True:
-        if num == 1 or num == 2 or num == 3 or num == 4: break
-        else: num = input(('숫자 1,2,3,4 중 다시 입력하세요'))
-    return str(2019+num)
