e9c975bf96ef41f32048d209a648d4b1f9720783
Hong Donghwan <colin12131@gmail.com>
2024-12-05 17:43:30 +0900

    np배열로 전달

--- ver 1.1/COSE_1201_2024320120.py
+++ ver 1.1/COSE_1201_2024320120.py
@@ -1,4 +1,6 @@
 #파일명 == 'C0SE_1201_2024320120'
+import numpy as np
+
 
 #과목 선택 함수, return 과목
 def print_subjects(subjects):
@@ -10,14 +12,39 @@ def print_subjects(subjects):
         else: print('다시 입력하세요.')
     return search
 
-#점수 딕셔너리 생성 함수, return 딕셔너리
-def create_dict(Data,subject,key):
+#영역 선택 함수, return 영역
+def organize_category (content_list) :
+    category = []
+    for line in content_list:
+        if line[0] not in category and line[0] != '영역': category.append(line[0])
+    print('영역 목록')
+    print('/'.join(category))
+    while(True):
+        search=input('영역을 선택하세요: ')
+        if search in category: break
+        else: print('다시 입력하세요.')
+    return search
 
-    result={}
+#과목 선택 함수, return 과목
+def organize_subject (content_list, category):
+    subjects = []
+    for line in content_list:
+        if line[0] == category and line[1] not in subjects : subjects.append(line[1])
+    print('<과목 목록>')
+    print('/'.join(subjects))
+    while(True):
+        search=input('과목을 선택하세요: ')
+        if search in subjects: break
+        else: print('다시 입력하세요.')
+    return search
+
+#점수 배열 생성 함수, return np배열
+def create_array(Data,subject,key):
     idx=Data[0].index(key)
+    result=[]
 
-    for score in Data:
-        if score[1]==subject:
-            result[int(score[2])]=int(score[idx])
+    for type in Data[1:]:
+        if type[1]==subject:
+            result.append(type[idx])
 
-    return result
+    return np.array(result)
